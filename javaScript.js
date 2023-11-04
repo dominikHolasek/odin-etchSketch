2c5f2f41742a93c88038cb09211416dbf7ee3f0e
dominikHolasek <101818170+dominikHolasek@users.noreply.github.com>
2023-11-04 11:57:22 +0100

    Add board size change mechanism

--- javaScript.js
+++ javaScript.js
@@ -1,27 +1,73 @@
 const board = document.querySelector('#hero');
+const sizeButton = document.querySelector('#square_size');
 
-const gridSize = 16;
 
-for (let i = 0; i < gridSize; i++){    
-    for(let x = 0; x < gridSize; x++){
-        let xDiv = document.createElement('div');
-        xDiv.classList.add('xAxisDiv');
-        xDiv.textContent = '';
-        board.appendChild(xDiv);
+let gridSize = 32;
+let divSize = 37.5;
+
+function setDivGridSize(){
+    if (gridSize < 16){
+        gridSize = 16;
+    }
+    
+    if (gridSize > 100){
+        gridSize = 100;
+    }
+    
+    divSize = 960/gridSize;
+}
+
+function createBoard(){
+    for (let i = 0; i < gridSize; i++){    
+        for(let x = 0; x < gridSize; x++){
+            let xDiv = document.createElement('div');
+            xDiv.classList.add('xAxisDiv');
+            xDiv.textContent = '';
+            board.appendChild(xDiv);
+        }
     }
 }
 
-const boardDivs = Array.from(document.querySelectorAll(".xAxisDiv"));  
-
-console.log(boardDivs[0]);
-for(let i = 0; i < boardDivs.length; i++){
-    boardDivs[i].addEventListener("mouseover", () => {
-        boardDivs[i].style.backgroundColor = "black";
-    })
+function game(){
+    setDivGridSize();
+    createBoard();
+    let boardDiv = document.querySelectorAll('.xAxisDiv');
+    boardDiv.forEach(element => {
+        element.style.height = divSize + "px";
+        element.style.width = divSize + "px";
+    });
+    
+    let boardDivs = Array.from(document.querySelectorAll(".xAxisDiv"));
+    console.log(boardDivs[0]);
+    for(let i = 0; i < boardDivs.length; i++){
+        boardDivs[i].addEventListener("mouseover", () => {
+            boardDivs[i].style.backgroundColor = "black";
+        })
+    } 
+    
+    function removeBoard(){
+        boardDiv.forEach(element => {
+            element.remove();
+        });
+    }
+    sizeButton.addEventListener('click', () => {
+        gridSize = +prompt('Write number from 8 to 64', '');
+        removeBoard();
+        setDivGridSize();
+        createBoard();
+        boardDiv = document.querySelectorAll('.xAxisDiv');
+        boardDiv.forEach(element => {
+            element.style.height = divSize + "px";
+            element.style.width = divSize + "px";
+        });
+        boardDivs = Array.from(document.querySelectorAll(".xAxisDiv"));
+        console.log(boardDivs[0]);
+        for(let i = 0; i < boardDivs.length; i++){
+            boardDivs[i].addEventListener("mouseover", () => {
+            boardDivs[i].style.backgroundColor = "black";
+            })
+        } 
+    });
 }
 
-/* boardDivs.forEach((e), () => {
-    e.addEventListener("mouseover", () => {
-        e.style.backround = "black";
-    })
-}) */
+game();
