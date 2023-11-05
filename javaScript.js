cdc0a2d31dea9537b63decc768dd67c323843c14
dominikHolasek <101818170+dominikHolasek@users.noreply.github.com>
2023-11-05 19:45:39 +0100

    Implement buttons click event

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,8 @@
 const board = document.querySelector('#hero');
 const sizeButton = document.querySelector('#square_size');
+const clearButton = document.querySelector('#clear');
+const rgbColorButton = document.querySelector('#rgb_color');
+const blackColorButton = document.querySelector('#black_color');
 
 
 let gridSize = 32;
@@ -7,15 +10,15 @@ let divSize = 37.5;
 let boardDiv = document.querySelectorAll('.xAxisDiv');
 let boardDivs = Array.from(document.querySelectorAll(".xAxisDiv"));
 let count = +0;
-let isRGBColor = false;
+let isRGBColor = true;
 
 function setDivGridSize(){
-    if (gridSize < 16){
-        gridSize = 16;
+    if (gridSize < 8){
+        gridSize = 8;
     }
     
-    if (gridSize > 100){
-        gridSize = 100;
+    if (gridSize > 64){
+        gridSize = 64;
     }
     
     divSize = 960/gridSize;
@@ -45,7 +48,18 @@ function boadrDivMoseOver(){
     console.log(boardDivs[0]);
     for(let i = 0; i < boardDivs.length; i++){
         boardDivs[i].addEventListener("mouseover", () => {
-            boardDivs[i].style.backgroundColor = "rgba(0,0,0," + (count / 10) + ")";
+
+            if (isRGBColor){
+                let red = +Math.round((Math.random() * 255));
+                let green = +Math.round((Math.random() * 255));
+                let blue = +Math.round((Math.random() * 255));
+
+
+                boardDivs[i].style.backgroundColor = `rgba(${red},${green},${blue},${(count/10)})` 
+            }else{
+                boardDivs[i].style.backgroundColor = "rgba(0,0,0," + (count / 10 + 0.1) + ")";
+            }
+            
             count++;
         })
     } 
@@ -57,12 +71,7 @@ function removeBoard(){
     });
 }
 
-function game(){
-    setDivGridSize();
-    createBoard();
-    setBoardDivSize();
-    boadrDivMoseOver();
-    
+function buttons(){
     sizeButton.addEventListener('click', () => {
         gridSize = +prompt('Write number from 8 to 64', '');
         count = 0;
@@ -72,6 +81,30 @@ function game(){
         setBoardDivSize();
         boadrDivMoseOver();
     });
+
+    rgbColorButton.addEventListener('click', () => {
+        isRGBColor = true;
+    });
+
+    blackColorButton.addEventListener('click', () => {
+        isRGBColor = false;
+    });
+
+    clearButton.addEventListener('click', () => {
+        count = 0;
+        removeBoard();
+        createBoard();
+        setBoardDivSize();
+        boadrDivMoseOver();
+    })
+}
+
+function game(){
+    setDivGridSize();
+    createBoard();
+    setBoardDivSize();
+    boadrDivMoseOver();
+    buttons();   
 }
 
 game();
